bbc71e62ff81f58c33cb64093fefbcad79cabb56
ramalabeysekera <ramal.abeysekera@hotmail.com>
2025-07-14 23:03:32 +0100

    Moved kubeconfig init to a separate file

--- pkg/k8s/createNamespace.go
+++ pkg/k8s/createNamespace.go
@@ -2,36 +2,13 @@ package k8s
 
 import (
 	"context"
-	"flag"
 	"fmt"
-	"path/filepath"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
 )
-
-var config *rest.Config
-var err error
-
-func init() {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-
-	config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
-	}
-}
 
 func CreateNamespace(namespace string) error {
 	fmt.Printf("Creating namespace: %s\n", namespace)
